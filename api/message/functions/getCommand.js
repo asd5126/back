305b9f1f0abf5debb3b9e2455d6bd8c90bfbffb1
Seungwoo Paek <seungwoopaek@sunssc.com>
2021-11-26 13:01:34 +0900

    커맨드 변경

--- api/message/functions/getCommand.js
+++ api/message/functions/getCommand.js
@@ -5,7 +5,10 @@ module.exports = async (trx, body) => {
     result: "SUCCESS",
     message: `
 ${COMMAND_PREFIX}명령어모음
-${COMMAND_PREFIX}지갑등록 [지갑주소]
-${COMMAND_PREFIX}포인트조회`.trim(),
+${COMMAND_PREFIX}지갑등록 지갑주소
+${COMMAND_PREFIX}포인트조회
+${COMMAND_PREFIX}방장조회
+${COMMAND_PREFIX}차감 유저이름 포인트 (방장전용)
+`.trim(),
   };
 };
