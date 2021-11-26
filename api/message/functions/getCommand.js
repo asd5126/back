cdf9099b918b31bbb3f508ee398734906cb220a7
Seungwoo Paek <seungwoopaek@sunssc.com>
2021-11-26 13:02:09 +0900

    명령어 변경

--- api/message/functions/getCommand.js
+++ api/message/functions/getCommand.js
@@ -4,7 +4,7 @@ module.exports = async (trx, body) => {
   return {
     result: "SUCCESS",
     message: `
-${COMMAND_PREFIX}명령어모음
+${COMMAND_PREFIX}명령어
 ${COMMAND_PREFIX}지갑등록 지갑주소
 ${COMMAND_PREFIX}포인트조회
 ${COMMAND_PREFIX}방장조회
